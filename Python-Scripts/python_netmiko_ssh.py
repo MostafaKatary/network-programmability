f6b24e0b7c97462771ef318ac8709d9cc8a359ae
MostafaKatary <mostafa.katary@gmail.com>
2020-01-12 13:05:30 +0200

    adding helper and refactoring code

--- Python-Scripts/python_netmiko_ssh.py
+++ Python-Scripts/python_netmiko_ssh.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-from copy import deepcopy
-# from pprint import pprint
-
 import netmiko
-import yaml
+
+from helper import read_pyaml, form_connection_parameters_from_yaml
 
 Vendor_Type = 'Cisco'
 
@@ -20,43 +18,6 @@ COMMANDS_LIST_JUNIPER = {
     'show system users',
     'show interfaces terse | match inet'
 }
-
-
-def read_pyaml(path='inventory.yml'):
-    """
-    Reads inventory yaml file and return dictionary with parsed values
-    Args:
-        path (str): path to inventory YAML
-
-    Returns:
-        dict: parsed inventory YAML values
-
-    """
-    with open(path) as file:
-        yaml_content = yaml.load(file.read(), Loader=yaml.FullLoader)
-    return yaml_content
-
-
-def form_connection_parameters_from_yaml(parsed_yaml, vendor='all'):
-    """
-    Form dictionary of netmiko connections parameters for all devices on site
-    
-    Args:
-        parsed_yaml (dict): dictionary with parsed yaml file
-        vendor (str): name of site. Default is 'all'
-    Returns:
-        dict: key is hostname, value is dict containing netmiko connection parameters for the host
-    """
-    parsed_yaml = deepcopy(parsed_yaml)
-    global_params = parsed_yaml['all']['vars']
-    site_dict = parsed_yaml['all']['groups'].get(vendor)
-    if site_dict is None:
-        raise KeyError('Site {} is not specified in inventory.yml'.format(vendor))
-    for host in site_dict['hosts']:
-        host_dict = {}
-        host_dict.update(global_params)
-        host_dict.update(host)
-        yield host_dict
 
 
 def collect_outputs(devices, commands):
